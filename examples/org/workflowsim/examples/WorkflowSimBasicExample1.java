da8f97c2c80f60de198705170fd09be9de6dbaed
zhedasuiyuan <huzhiming.cs.zju@gmail.com>
2013-11-12 23:11:52 +0800

    commit

--- examples/org/workflowsim/examples/WorkflowSimBasicExample1.java
+++ examples/org/workflowsim/examples/WorkflowSimBasicExample1.java
@@ -107,7 +107,7 @@ public class WorkflowSimBasicExample1 {
             /**
              * Should change this based on real physical path
              */
-            String daxPath = "/Users/chenweiwei/Work/WorkflowSim-1.0/config/dax/Montage_100.xml";
+            String daxPath = "C:\\Users\\zhiming\\git\\WorkflowSim-1.0\\config\\dax\\Montage_100.xml";
             if(daxPath == null){
                 Log.printLine("Warning: Please replace daxPath with the physical path in your working environment!");
                 return;
